f338df353d72336998207668ce127cc119f683ed
Daniel <danielusovdev@gmail.com>
2024-09-17 16:04:53 +0300

    fix: swagger plugin

--- src/plugins/swagger.ts
+++ src/plugins/swagger.ts
@@ -1,9 +1,8 @@
 import fastifySwagger from '@fastify/swagger';
 import fastifySwaggerUi from '@fastify/swagger-ui';
-import apiReference from '@scalar/fastify-api-reference';
 import fp from 'fastify-plugin';
-import { apiTags } from './../configs/apiTags';
 import { version } from '../../package.json';
+import { apiTags } from './../configs/apiTags';
 
 export default fp(async fastify => {
   fastify.register(fastifySwagger, {
@@ -11,8 +10,8 @@ export default fp(async fastify => {
       info: {
         title: 'KTrek API Gateway',
         version
-      }
-      // tags: apiTags
+      },
+      tags: apiTags
     }
   });
 
@@ -24,21 +23,4 @@ export default fp(async fastify => {
     staticCSP: true,
     transformStaticCSP: header => header
   });
-
-  // fastify.register(apiReference, {
-  //   routePrefix: '/reference',
-  //   configuration: {
-  //     spec: {
-  //       content: () => fastify.swagger()
-  //     }
-  //   },
-  //   hooks: {
-  //     onRequest: function (request, reply, done) {
-  //       done();
-  //     },
-  //     preHandler: function (request, reply, done) {
-  //       done();
-  //     }
-  //   }
-  // });
 });
